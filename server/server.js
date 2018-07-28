09765e7bfa7884420c3a7f0bcc099b331f97627f
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-07-28 18:47:22 -0500

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose.js');
 const { Todo } = require('./models/todo.js');
@@ -67,6 +68,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime(); //returns javascript timestamp.
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true } ).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }, (err) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
