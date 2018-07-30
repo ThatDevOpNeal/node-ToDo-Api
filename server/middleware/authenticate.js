a53eb6fcd6b7bf4c1f56a3e65e36d2db82ebc7c5
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-07-29 19:51:17 -0500

    Add GET /users/me and auth middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,21 @@
+const { User } = require('./../models/user');
+
+var authenticate = (req, res, next) => {
+    const token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject(); // will make it hit the error case (.catch)
+        }
+        
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((err) => {
+        res.status(401).send();
+    });
+}
+
+module.exports = {
+    authenticate
+};
